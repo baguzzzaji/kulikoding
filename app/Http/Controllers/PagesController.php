a2d7c722662df7879ab8be2d96daee3ca3f84a3f
Bagus Aji Santoso <baguzzzaji@gmail.com>
2017-01-01 16:56:49 +0700

    forget to save file

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,8 +13,4 @@ class PagesController extends Controller
     public function about() {
         return view('about');
     }
-
-    public function submit() {
-        return view('submit');
-    }
 }
